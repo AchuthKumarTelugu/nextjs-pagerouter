637dadd979d923b18b1a5a9b6ca69457a4104441
AchuthKumarTelugu <achuthkumar.telugu123@gmail.com>
2024-04-01 20:24:33 +0530

    fs.readdir

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,38 +1,22 @@
-import fs, { read } from 'fs'
+import fs, { read } from 'fs/promises'
 import path from 'path';
 export default async function handler(req, res) {
-  // const filePath = path.join(process.cwd(), 'blogdata')
-  // let resArr = []
-  // fs.readdir(filePath, (err, data) => {
-  //   if (err) return res.status(500).json({ msg: "no such blogs found" })
-  //   // console.log(data)
-  //   data.forEach((value) => {
-  //     const str = filePath + "\\" + value
-  //     fs.readFile(str, (err, file) => {
-  //       // console.log(file.toString())
-  //       resArr.push(file.toString())
-  //     })
-  //   })
-  //   console.log('resArr', resArr)
-  //   res.status(200).json(data)
-  //   // res.status(200).json(resArr)
 
   // })//we recieve data in string
   const filePath = path.join(process.cwd(), 'blogdata')
   let resArr = []
   try {
-    fs.readdir(filePath, (err, data) => {
-      if (err) return res.status(500).json({ msg: "blogs not found" })
-      for (let file of data) {
-        let newPath = path.join(filePath,file)
-        const obj = fs.readFileSync(newPath, "utf-8")
-        // console.log(obj)
-        resArr.push(obj)
-      }
-    })
-    console.log('resArr',resArr)
-    res.status(200).json({msg:"hello"})
-  } catch (error) {
+    const files = await fs.readdir(filePath)
+    for (let file of files) {
+      let newPath = path.join(filePath, file)
+      const obj = await fs.readFile(newPath, "utf-8")
+      // console.log(obj)
+      resArr.push(JSON.parse(obj))
+    }
+    console.log('resArr',resArr )
+      res.status(200).json(resArr)
+  }
+  catch (error) {
     console.log(error)
   }
 
