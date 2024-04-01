d1a83d444fd030acaa967e4cc7a676cbf4e6bd8a
AchuthKumarTelugu <achuthkumar.telugu123@gmail.com>
2024-04-01 19:23:48 +0530

    getBlogdata added

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
 
 const blog = () => {
-    
+    //get all data from blogdata directory
+    //iterate throught them and display them
   return (
-    <div>
-     this is blog home page
+    <div className='grid place-content-center '>
+     <div className="w-1/2 m-auto" >
+        <h1 className="font-bold text-3xl pt-8 pb-4 text-center">Latest Blogs</h1>
+        <div className="blog-section grid grid-cols-2  gap-4 py-5">
+          <div className="blog">
+            <h2 className="font-bold text-xl"> <Link href={'/blogpost/how-to-learn-javascript'} > How to learn javascript ? </Link></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laborum tenetur possimus dolore eos quo atque eum. Vero, enim. Impedit.</p>
+          </div>
+          <div className="blog">
+            <h2 className="font-bold text-xl">How to learn javascript ?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laborum tenetur possimus dolore eos quo atque eum. Vero, enim. Impedit.</p>
+          </div>
+          <div className="blog">
+            <h2 className="font-bold text-xl">How to learn javascript ?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laborum tenetur possimus dolore eos quo atque eum. Vero, enim. Impedit.</p>
+          </div>
+          <div className="blog">
+            <h2 className="font-bold text-xl">How to learn javascript ?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, laborum tenetur possimus dolore eos quo atque eum. Vero, enim. Impedit.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,13 +1,18 @@
 import { useRouter } from 'next/router'
 import React from 'react'
-
+//find the document corresponding the slug
+//iterate through them and display it
 const slug = () => {
     const router=useRouter()
     const {slug}=router.query
     console.log(slug)
   return (
     <div>
-      <h1>title name:{slug}</h1>
+      <div className='w-1/2 mx-auto text-center mt-5 grid gap-3'>
+      <h1 className='font-bold text-3xl'>Title name:{slug}</h1>
+      <hr className='border-black'/>
+      <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam enim expedita maxime quae quas exercitationem ratione molestias temporibus, nam reprehenderit?</p>
+      </div>
     </div>
   )
 }
