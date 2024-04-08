067212a32df722bb145891f1d2b471f5ccda06b3
AchuthKumarTelugu <achuthkumar.telugu123@gmail.com>
2024-04-08 16:44:45 +0530

    antd

--- pages/api/postContact.js
+++ pages/api/postContact.js
@@ -1,6 +1,11 @@
-export default function handler(req,res){
+import fs from 'fs'
+export default async function handler(req,res){
     if(req.method=="POST") {
-        res.status(200).json(["post request from postContact is cleared"])
+        console.log(req.body)
+        const data=await fs.promises.readdir('contactData')
+        console.log(data,data.length)
+       const result= await fs.promises.writeFile(`contactData/info${data.length + 1}.json`,JSON.stringify(req.body,null,2))//based on files length,we increment file name
+        res.status(200).json({msg:'info written succesfully',result})
     }else{
         res.status(200).json(["this is message from post-contact request"])
     }
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import {Form,Input} from  "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons'
 const contact = () => {
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
+  function submitData(v) {
+
+  }
   return (
     <div>
-      this is contact page
+      <div className='w-1/2 mx-auto bg-black color-white'>
+      <h1 className='text-center text-4xl text-pink-400 p-4 font-bold uppercase my-5'>This is contact page</h1>
+      
+
+      </div>
     </div>
   )
 }
